1c6fa91971e5d7fd2096cd6b20c038c67dc95e50
Mario Gonzalez <mario.gonzalez@berkeley.edu>
2026-02-20 03:14:06 -0800

    adding cli capability to probe.c

--- src/probe.c
+++ src/probe.c
@@ -23,7 +23,7 @@ static uint16_t le16(const uint8_t *p) {
 	return (uint16_t)p[0] | ((uint16_t)p[1] << 8);
 }
 
-// interpret 18-byte USB Device Descriptor layout/configuration
+// list out 18-byte USB device descriptors
 static void dump_device_descriptor(const int8_t d[18]) {
 	printf("Device Descriptor:\n");
 	printf("  bdUSB		: %x.%02x\n", d[2], d[3]);
@@ -46,21 +46,55 @@ static void parse_cfg(const uint8_t *cfg, size_t n) {
 
 		if (bLength == 0 || i + bLength > n) break;
 
-		/*switch bType*/
-		if (bType == 2 && bLength >= 9) { //CONFIG: overall total length and interface count
+		// CONFIG descriptor listing total length and interface count
+		if (bType == 2 && bLength >= 9) {
 			printf("  CONFIG: wTotalLength=%u, bNumInterfaces=%u\n", le16(&cfg[i + 2]), cfg[i + 4]);
+		
+		// INTERFACE number, alt setting, endpoint count, class codes 
 		} else if (bType == 4 && bLength >= 9) {
 			cur_iface = cfg[i + 2];
 			printf("  INTERFACE: %d, alt=%u, eps=%u, class=%u%u%u\n", cur_iface,
-					cfg[i + 3], cfg[i + 4], cfg[i + 5], cfg[i +6], cfg[i + 7]);
+					cfg[i + 3], cfg[i + 4], cfg[i + 5], 
+					cfg[i +6], cfg[i + 7]);
+
+		// ENDPOINT address, transfer type, max packet size
 		} else if (btype == 5 && bLength >= 7) {
 			uint8_t ep = cfg[i +2];
 			uint8_t attr = cfg[i +3];
 			uint8_t mps = le16(&cfg[i + 4]);
-			uint8_t ivl = cfg[i + 6];
+			uint8_t intvl = cfg[i + 6];
 
+			// bit 7 indicates direction of communcation between host/device
+			const char *dir = (ep & 0x80) ? "IN" : "OUT";
+			uint8_t xfertype = attr & 0x03;
+			const char *t = (xfertype == 0) ? "CTRL" : 
+					(xfertype == 1) ? "ISOC" :
+					(xfertype == 2) ? "BULK" : "INTR";
+
+			printf("	EP %0x02x (%s): (%s), mps=%u, interval=%u\n", ep, dir, t, mps, intvl);
 
 		}
 	}
+}
+
+int main(int argc, char **argv) {
+	uint8_t vid = 0, pid = 0;
+	int claim0 = 0;
+
+	/* CLI Usage:
+	 * ./probe		--> scan all, open first
+	 * ./probe 045e 02ae 	--> filter vid, pid
+	 * ./probe 045e 02ae --claim 0 */
+	
+	 if (argc >= 3){
+		 vid = (uint16_t)strtoul(argv[1], NULL, 16);
+		 pid = (uint16_t)stroul(argv[2], NULL, 16);
+	 }
+	 if (argc >= 4 && strcmp(argv[3], "--claim0") == 0) claim0 = 1;
+
+	 kn_usb *ids = NULL;
+	 CHECK(kn_usb_new(&u));
+
+
 }
 
