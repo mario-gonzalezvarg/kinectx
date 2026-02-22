a98a705733a0d77a4351bb05bcbc4025ec2ec00f
Mario Gonzalez <mario.gonzalez@berkeley.edu>
2026-02-21 18:39:27 -0800

    modular state, resource handler, and error code mapping to libusb library added to usb.c

--- src/usb.c
+++ src/usb.c
@@ -1,1 +1,49 @@
 //usb.c
+#include "usb.h"
+
+#include <stdlib.h>
+#include <string.h>
+#include <sys/time.h>
+
+#include <libusb-1.0/libusb.h>
+
+// module state
+struct device_host {
+    libusb_context *usb;
+};
+
+// resource handler
+struct device_link {
+    device_host *host;
+    libusb_device_handle *usb;
+    uint32_t claimed;
+    uint32_t detached;
+};
+
+static int map_libusb(int rc) {
+  if (rc >= 0) return rc;
+  switch (rc) {
+    case LIBUSB_ERROR_INVALID_PARAM: return DEVICE_EINVAL;
+    case LIBUSB_ERROR_NO_MEM:        return DEVICE_ENOMEM;
+    case LIBUSB_ERROR_NO_DEVICE:     return DEVICE_ENODEV;
+    case LIBUSB_ERROR_TIMEOUT:       return DEVICE_ETIME;
+    case LIBUSB_ERROR_BUSY:          return DEVICE_EBUSY;
+    case LIBUSB_ERROR_ACCESS:        return DEVICE_EACCESS;
+    default:                         return DEVICE_EIO;
+  }
+}
+
+const char *device_err_str(int code) {
+  switch (code) {
+    case DEVICE_OK:      return "ok";
+    case DEVICE_EINVAL:  return "invalid argument";
+    case DEVICE_ENOMEM:  return "out of memory";
+    case DEVICE_EIO:     return "I/O error";
+    case DEVICE_ETIME:   return "timeout";
+    case DEVICE_ENODEV:  return "no such device";
+    case DEVICE_EBUSY:   return "busy";
+    case DEVICE_EACCESS: return "access denied";
+    case DEVICE_ESTATE:  return "invalid state";
+    default:             return "unknown error";
+  }
+}
--- src/usb.h
+++ src/usb.h
@@ -20,7 +20,7 @@ typedef struct {
 /* decoding layer for operation success/failure */
 enum {
 	DEVICE_OK = 0,
-	DEVICE_INVAL = -1,
+	DEVICE_EINVAL = -1,
 	DEVICE_ENOMEM = -2,
 	DEVICE_EIO = -3,
 	DEVICE_ETIME = -4,
@@ -37,11 +37,11 @@ int device_host_create(device_host **out);
 void device_host_destroy(device_host *host);
 
 /* enumerate devices connected to USB */
-int device_host_scan(device_host *host, uint16_t vid, uint16_t pid, device_ids **out_ids, size_t *out_n);
-void device_ids_destroy(device_ids *ids);
+int device_host_scan(device_host *host, uint16_t vid, uint16_t pid, device_id **out_ids, size_t *out_n);
+void device_ids_destroy(device_id *ids);
 
 /* open/close link handle */
-int device_link_open(device_host *host, const device_id *id, device_lin **out_link);
+int device_link_open(device_host *host, const device_id *id, device_link **out_link);
 void device_link_close(device_link *link);
 
 /* interface management */
@@ -50,8 +50,8 @@ int device_link_release(device_link *link, int iface);
 int device_link_set_alt(device_link *link, int iface, int alt);
 
 /* transfers */
-int device_link_ctrl(device_linkk *link, uint8_t bmReq, uint8_t bReq, uint16_t wVal, uint16_t wIdx, void *data, unsigned timeout_ms);
-iny device_link_bulk(device_link *link, uint8_t ep, void *data, unsigned timeout_ms);
+int device_link_ctrl(device_link *link, uint8_t bmReq, uint8_t bReq, uint16_t wVal, uint16_t wIdx, void *data, unsigned timeout_ms);
+int device_link_bulk(device_link *link, uint8_t ep, void *data, unsigned timeout_ms);
 
 /* event pump */
 int device_host_poll(device_host *host, int timeout_ms);
